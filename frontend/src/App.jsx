5e6d0320a85b798cee6acbd267706c36f3bea254
anish <anishkr0098@gmail.com>
2026-03-02 11:16:53 +0530

    CreateCourses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,11 +13,13 @@ import EditProfile from './pages/EditProfile'
 import Dashboard from './pages/Educator/Dashboard'
 import Courses from './pages/Educator/Courses'
 import CreateCourses from './pages/Educator/CreateCourses'
+import GetCreatorCourse from "./customHooks/GetCreatorCourse"
 
 export const serverUrl = "http://localhost:8000"
 
 function App() {
   GetCurrentuser();
+  GetCreatorCourse();
   const {userData} = useSelector(state=>state.user);
   return (
     <>
@@ -32,6 +34,7 @@ function App() {
          <Route path='/dashboard' element={userData?.role === "educator"?<Dashboard/> :<Navigate to={'/signup'} />}/>
          <Route path='/courses' element={userData?.role === "educator"?<Courses/> :<Navigate to={'/signup'} />}/>
          <Route path='/createcourses' element={userData?.role === "educator"?<CreateCourses/> :<Navigate to={'/signup'} />}/>
+         <Route path='/editcourse/:courseId' element={userData?.role === "educator"?<CreateCourses/> :<Navigate to={'/signup'} />}/>
       </Routes>
       
     </>
--- frontend/src/pages/Educator/Courses.jsx
+++ frontend/src/pages/Educator/Courses.jsx
@@ -3,10 +3,14 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
 import img from "../../assets/empty.jpg";
 import { FaEdit } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 
 
 function Courses() {
     const navigate = useNavigate();
+    // map course creator
+    const {creatorCourseData} = useSelector(state=>state.course);
+
   return (
     <div className='flex min-h-screen bg-gray-100'>
         <div className='w-[100%] min-h-screen p-4 sm:p-6 bg-gray-100'>
@@ -30,32 +34,39 @@ function Courses() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className='border-b hover:bg-gray-50 transition duration-200'>
+                    {creatorCourseData?.map((course,index) =>(
+                    <tr key={index} className='border-b hover:bg-gray-50 transition duration-200'>
                         <td className='py-3 px-4 flex items-center gap-4'>
-                            <img src={img} className='w-25 h-14 object-cover rounded-md' alt="img" />
-                            <span>Title</span>
+                            {course?.thumbnail?<img src={course?.thumbnail} className='w-25 h-14 object-cover rounded-md' alt="img" />:<img src={img} className='w-25 h-14 object-cover rounded-md' alt="img" />}
+                            <span>{course.title}</span>
                         </td>
-                        <td className='px-4 py-3'>$ NA</td>
-                        <td className='px-4 py-3'><span className='px-3 py-1 rounded-full text-xs bg-red-100 text-red-600'>Draft</span></td>
-                        <td className='px-4 py-3'><FaEdit className='text-gray-600 hover:text-blue-600 cursor-pointer' /></td>
+                        {course?.price ?<td className='px-4 py-3'>${course?.price}</td>:<td className='px-4 py-3'>$ NA</td>}
+                        <td className='px-4 py-3'><span className={`px-3 py-1 rounded-full text-xs ${course.isPublished? "bg-green-100 text-green-600": "bg-red-100 text-red-600"}`}>{course?.isPublished?"Published": "Draft"}</span></td>
+                        <td className='px-4 py-3'><FaEdit className='text-gray-600 hover:text-blue-600 cursor-pointer' onClick={() =>navigate(`/editcourse/${course?._id}`)} /></td>
                     </tr>
+                     ))}
                 </tbody>
             </table>
             <p className='text-center text-sm text-gray-400 mt-6'>A list of your recent courses.</p>
           </div>
           {/* for small screen table */}
-          <div className='md:hidden space-y-4'>
+          <div className='md:hidden space-y-4 mt-3'>
+           {creatorCourseData?.map((course,index) =>(
+            <div key={index} className='bg-white rounded-lg shadow p-4 flex flex-col gap-3'>
             <div className='flex gap-4 items-center mt-4'>
-                <img src={img} className='w-16 h-16 rounded-md object-cover' alt="img"/>
+              {  course?.thumbnail?<img src={course?.thumbnail} className='w-16 h-16 rounded-md object-cover' alt="img"/>:
+                <img src={img} className='w-16 h-16 rounded-md object-cover' alt="img"/>}
                 <div className='flex-1'>
-                    <h2 className='font-medium text-sm'>Title</h2>
-                    <p className='text-gray-600 text-xs mt-1'>$ NA</p>
+                    <h2 className='font-medium text-sm'>{course.title}</h2>
+                    {course?.price ?<p className='text-gray-600 text-xs mt-1'>${course?.price}</p>:
+                    <p className='text-gray-600 text-xs mt-1'>$ NA</p>}
                 </div>
-                <FaEdit className='text-gray-600 hover:text-blue-600 cursor-pointer' />
+                <FaEdit className='text-gray-600 hover:text-blue-600 cursor-pointer' onClick={() =>navigate(`/editcourse/${course?._id}`)} />
             </div>
-            <span className='w-fit px-3 py-1 text-xs rounded-full bg-red-100 text-red-600'>Draft</span>
+            <span className={`w-fit px-3 py-1 text-xs rounded-full ${course?.isPublished? "bg-green-100 text-green-600":"bg-red-100 text-red-600"} `}>{course?.isPublished? "Published":"Draft"}</span>
+        </div> ))}
           <p className='text-center text-sm text-gray-400 mt-4'>A list of your recent courses.</p>
-          </div>
+        </div> 
         </div>
     </div>
   )
