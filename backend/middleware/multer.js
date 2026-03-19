791e0dffac4b2ef96d69b58aaf4fddd063a2e2c9
anish <anishkr0098@gmail.com>
2026-03-19 13:46:20 +0530

    public

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,15 +1,23 @@
+// middleware/multer.js
 import multer from "multer";
+import fs from "fs";
 
-// storage create
+const uploadDir = "./public";
+
+// ✅ create folder if not exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 let storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null, "./public")
-    },
-    filename:(req,file,cb)=>{
-        cb(null, file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // 🔥 avoid overwrite
+  },
+});
 
-})
+const upload = multer({ storage });
 
-const upload = multer({storage})
-export default upload
+export default upload;
